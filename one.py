12ece24601ae9f73b9c01acb66c574082c1760d3
r3card0 <ricardo.rdz@pm.me>
2022-09-28 22:25:38 -0500

    ne licke function

--- one.py
+++ one.py
@@ -1,5 +1,17 @@
 import sys
 
+
+def system_version():
+    sv = sys.version
+    return sv
+
+def run():
+    print(system_version())
+
+
+if __name__ == "__main__":
+    run()
+
 #print(sys.version)
 
 print(sys.argv)
